7d7edf9e8839f6077460c1734c5ecda663af77cf
Klaus Niedermair <klaus.niedermair@quickbirdmedical.com>
2023-03-29 12:05:46 +0200

    feat: implement propr score calculation in typescript

---
+++ propr.ts
@@ -0,0 +1,312 @@
+const to_dead = 1.021915
+const turncog1 = -2.052
+const turncog2 = -1.565
+const turncog3 = -1.239
+const turncog4 = -0.902
+const turncog5 = -0.649
+const turncog6 = -0.367
+const turncog7 = -0.002
+const turncog8 = 0.52
+const turncog9 = 1.124
+const turndep1 = -1.082
+const turndep2 = -0.264
+const turndep3 = 0.151
+const turndep4 = 0.596
+const turndep5 = 0.913
+const turndep6 = 1.388
+const turndep7 = 1.742
+const turndep8 = 2.245
+const turndep9 = 2.703
+const turnfat1 = -1.648
+const turnfat2 = -0.818
+const turnfat3 = -0.094
+const turnfat4 = 0.303
+const turnfat5 = 0.87
+const turnfat6 = 1.124
+const turnfat7 = 1.688
+const turnfat8 = 2.053
+const turnfat9 = 2.423
+const turnpain1 = -0.773
+const turnpain2 = 0.1
+const turnpain3 = 0.462
+const turnpain4 = 0.827
+const turnpain5 = 1.072
+const turnpain6 = 1.407
+const turnpain7 = 1.724
+const turnpain8 = 2.169
+const turnpain9 = 2.725
+const turnphys1 = -2.575
+const turnphys2 = -2.174
+const turnphys3 = -1.784
+const turnphys4 = -1.377
+const turnphys5 = -0.787
+const turnphys6 = -0.443
+const turnphys7 = -0.211
+const turnphys8 = 0.16
+const turnphys9 = 0.966
+const turnsleep1 = -1.535
+const turnsleep2 = -0.775
+const turnsleep3 = -0.459
+const turnsleep4 = 0.093
+const turnsleep5 = 0.335
+const turnsleep6 = 0.82
+const turnsleep7 = 1.659
+const turnsleep8 = 1.934
+const turnsocial1 = -2.088
+const turnsocial2 = -1.634
+const turnsocial3 = -1.293
+const turnsocial4 = -0.955
+const turnsocial5 = -0.618
+const turnsocial6 = -0.276
+const turnsocial7 = 0.083
+const turnsocial8 = 0.494
+const turnsocial9 = 1.221
+const slopecog1 = -1.0047
+const slopecog2 = -0.1745
+const slopecog3 = -0.4223
+const slopecog4 = -0.1949
+const slopecog5 = -0.1082
+const slopecog6 = -0.2468
+const slopecog7 = -0.0176
+const slopecog8 = -0.2192
+const slopedep1 = 0.1572
+const slopedep2 = 0
+const slopedep3 = 0.1793
+const slopedep4 = 0.1817
+const slopedep5 = 0.4109
+const slopedep6 = 0.1887
+const slopedep7 = 0.2115
+const slopedep8 = 0.7983
+const slopefat1 = 0.1152
+const slopefat2 = 0.1077
+const slopefat3 = 0.1189
+const slopefat4 = 0.1277
+const slopefat5 = 0.222
+const slopefat6 = 0.0496
+const slopefat7 = 0.3233
+const slopefat8 = 1.3632
+const slopepain1 = 0.0891
+const slopepain2 = 0.1721
+const slopepain3 = 0.1022
+const slopepain4 = 0.4241
+const slopepain5 = 0.3815
+const slopepain6 = 0.3681
+const slopepain7 = 0.1169
+const slopepain8 = 0.7594
+const slopephys1 = -1.0761
+const slopephys2 = -0.1756
+const slopephys3 = -0.1764
+const slopephys4 = -0.1161
+const slopephys5 = -0.2721
+const slopephys6 = -0.4082
+const slopephys7 = -0.1695
+const slopephys8 = -0.1346
+const slopesleep1 = 0.1241
+const slopesleep2 = 0
+const slopesleep3 = 0.0797
+const slopesleep4 = 0.3455
+const slopesleep5 = 0.3148
+const slopesleep6 = 0.1238
+const slopesleep7 = 1.8964
+const slopesocial1 = -1.1152
+const slopesocial2 = -0.2874
+const slopesocial3 = -0.1352
+const slopesocial4 = -0.132
+const slopesocial5 = -0.4012
+const slopesocial6 = 0
+const slopesocial7 = -0.054
+const slopesocial8 = -0.201
+const interceptcog1 = -1.0617
+const interceptcog2 = 0.2375
+const interceptcog3 = -0.0694
+const interceptcog4 = 0.1357
+const interceptcog5 = 0.192
+const interceptcog6 = 0.1411
+const interceptcog7 = 0.1416
+const interceptcog8 = 0.2464
+const interceptdep1 = 0.1701
+const interceptdep2 = 0.1286
+const interceptdep3 = 0.1015
+const interceptdep4 = 0.1001
+const interceptdep5 = -0.1092
+const interceptdep6 = 0.1993
+const interceptdep7 = 0.1595
+const interceptdep8 = -1.1577
+const interceptfat1 = 0.1898
+const interceptfat2 = 0.1837
+const interceptfat3 = 0.1848
+const interceptfat4 = 0.1821
+const interceptfat5 = 0.1
+const interceptfat6 = 0.2938
+const interceptfat7 = -0.1681
+const interceptfat8 = -2.3031
+const interceptpain1 = 0.0689
+const interceptpain2 = 0.0606
+const interceptpain3 = 0.0929
+const interceptpain4 = -0.1733
+const interceptpain5 = -0.1277
+const interceptpain6 = -0.1089
+const interceptpain7 = 0.3243
+const interceptpain8 = -1.0692
+const interceptphys1 = -1.7709
+const interceptphys2 = 0.1867
+const interceptphys3 = 0.1853
+const interceptphys4 = 0.2683
+const interceptphys5 = 0.1456
+const interceptphys6 = 0.0853
+const interceptphys7 = 0.1356
+const interceptphys8 = 0.13
+const interceptsleep1 = 0.1905
+const interceptsleep2 = 0.0943
+const interceptsleep3 = 0.1309
+const interceptsleep4 = 0.1062
+const interceptsleep5 = 0.1164
+const interceptsleep6 = 0.2731
+const interceptsleep7 = -2.6676
+const interceptsocial1 = -1.3285
+const interceptsocial2 = 0.0241
+const interceptsocial3 = 0.2209
+const interceptsocial4 = 0.2239
+const interceptsocial5 = 0.0576
+const interceptsocial6 = 0.1683
+const interceptsocial7 = 0.1728
+const interceptsocial8 = 0.2454
+const c_cognition = 0.6350450
+const c_depression = 0.6661641
+const c_fatigue = 0.6386135
+const c_pain = 0.6529680
+const c_physical = 0.6883584
+const c_sleep = 0.5629657
+const c_social = 0.6112686
+const C = -0.9991828
+
+function propsScore(
+    theta_dep: number,
+    theta_fat: number,
+    theta_pain: number,
+    theta_phys: number,
+    theta_slp: number,
+    theta_sr: number,
+    theta_cog: number
+) {
+    let cog_disutility = 1
+    cog_disutility = interceptcog1 + theta_cog * (((turncog1 <= theta_cog) && (theta_cog < turncog2)) ? slopecog1 : cog_disutility);
+    cog_disutility = interceptcog2 + theta_cog * (((turncog2 <= theta_cog) && (theta_cog < turncog3)) ? slopecog2 : cog_disutility);
+    cog_disutility = interceptcog3 + theta_cog * (((turncog3 <= theta_cog) && (theta_cog < turncog4)) ? slopecog3 : cog_disutility);
+    cog_disutility = interceptcog4 + theta_cog * (((turncog4 <= theta_cog) && (theta_cog < turncog5)) ? slopecog4 : cog_disutility);
+    cog_disutility = interceptcog5 + theta_cog * (((turncog5 <= theta_cog) && (theta_cog < turncog6)) ? slopecog5 : cog_disutility);
+    cog_disutility = interceptcog6 + theta_cog * (((turncog6 <= theta_cog) && (theta_cog < turncog7)) ? slopecog6 : cog_disutility);
+    cog_disutility = interceptcog7 + theta_cog * (((turncog7 <= theta_cog) && (theta_cog < turncog8)) ? slopecog7 : cog_disutility);
+    cog_disutility = interceptcog8 + theta_cog * (((turncog8 <= theta_cog) && (theta_cog < turncog9)) ? slopecog8 : cog_disutility);
+    cog_disutility = (turncog9 <= theta_cog) ? 0 : cog_disutility
+
+    let dep_disutility = 0
+    dep_disutility = interceptdep1 + theta_dep * (((turndep1 <= theta_dep) && (theta_dep < turndep2)) ? slopedep1 : dep_disutility);
+    dep_disutility = interceptdep2 + theta_dep * (((turndep2 <= theta_dep) && (theta_dep < turndep3)) ? slopedep2 : dep_disutility);
+    dep_disutility = interceptdep3 + theta_dep * (((turndep3 <= theta_dep) && (theta_dep < turndep4)) ? slopedep3 : dep_disutility);
+    dep_disutility = interceptdep4 + theta_dep * (((turndep4 <= theta_dep) && (theta_dep < turndep5)) ? slopedep4 : dep_disutility);
+    dep_disutility = interceptdep5 + theta_dep * (((turndep5 <= theta_dep) && (theta_dep < turndep6)) ? slopedep5 : dep_disutility);
+    dep_disutility = interceptdep6 + theta_dep * (((turndep6 <= theta_dep) && (theta_dep < turndep7)) ? slopedep6 : dep_disutility);
+    dep_disutility = interceptdep7 + theta_dep * (((turndep7 <= theta_dep) && (theta_dep < turndep8)) ? slopedep7 : dep_disutility);
+    dep_disutility = interceptdep8 + theta_dep * (((turndep8 <= theta_dep) && (theta_dep < turndep9)) ? slopedep8 : dep_disutility);
+    dep_disutility = (turndep9 <= theta_dep) ? 1 : dep_disutility;
+
+    let fat_disutility = 0
+    fat_disutility = interceptfat1 + theta_fat * (((turnfat1 <= theta_fat) && (theta_fat < turnfat2)) ? slopefat1 : fat_disutility);
+    fat_disutility = interceptfat2 + theta_fat * (((turnfat2 <= theta_fat) && (theta_fat < turnfat3)) ? slopefat2 : fat_disutility);
+    fat_disutility = interceptfat3 + theta_fat * (((turnfat3 <= theta_fat) && (theta_fat < turnfat4)) ? slopefat3 : fat_disutility);
+    fat_disutility = interceptfat4 + theta_fat * (((turnfat4 <= theta_fat) && (theta_fat < turnfat5)) ? slopefat4 : fat_disutility);
+    fat_disutility = interceptfat5 + theta_fat * (((turnfat5 <= theta_fat) && (theta_fat < turnfat6)) ? slopefat5 : fat_disutility);
+    fat_disutility = interceptfat6 + theta_fat * (((turnfat6 <= theta_fat) && (theta_fat < turnfat7)) ? slopefat6 : fat_disutility);
+    fat_disutility = interceptfat7 + theta_fat * (((turnfat7 <= theta_fat) && (theta_fat < turnfat8)) ? slopefat7 : fat_disutility);
+    fat_disutility = interceptfat8 + theta_fat * (((turnfat8 <= theta_fat) && (theta_fat < turnfat9)) ? slopefat8 : fat_disutility);
+    fat_disutility = (turnfat9 <= theta_fat) ? 1 : fat_disutility;
+
+    let pain_disutility = 0
+    pain_disutility = interceptpain1 + theta_pain * (((turnpain1 <= theta_pain) && (theta_pain < turnpain2)) ? slopepain1 : pain_disutility);
+    pain_disutility = interceptpain2 + theta_pain * (((turnpain2 <= theta_pain) && (theta_pain < turnpain3)) ? slopepain2 : pain_disutility);
+    pain_disutility = interceptpain3 + theta_pain * (((turnpain3 <= theta_pain) && (theta_pain < turnpain4)) ? slopepain3 : pain_disutility);
+    pain_disutility = interceptpain4 + theta_pain * (((turnpain4 <= theta_pain) && (theta_pain < turnpain5)) ? slopepain4 : pain_disutility);
+    pain_disutility = interceptpain5 + theta_pain * (((turnpain5 <= theta_pain) && (theta_pain < turnpain6)) ? slopepain5 : pain_disutility);
+    pain_disutility = interceptpain6 + theta_pain * (((turnpain6 <= theta_pain) && (theta_pain < turnpain7)) ? slopepain6 : pain_disutility);
+    pain_disutility = interceptpain7 + theta_pain * (((turnpain7 <= theta_pain) && (theta_pain < turnpain8)) ? slopepain7 : pain_disutility);
+    pain_disutility = interceptpain8 + theta_pain * (((turnpain8 <= theta_pain) && (theta_pain < turnpain9)) ? slopepain8 : pain_disutility);
+    pain_disutility = (turnpain9 <= theta_pain) ? 1 : pain_disutility;
+
+    let physical_disutility = 1
+    physical_disutility = interceptphys1 + theta_phys * (((turnphys1 <= theta_phys) && (theta_phys < turnphys2)) ? slopephys1 : physical_disutility);
+    physical_disutility = interceptphys2 + theta_phys * (((turnphys2 <= theta_phys) && (theta_phys < turnphys3)) ? slopephys2 : physical_disutility);
+    physical_disutility = interceptphys3 + theta_phys * (((turnphys3 <= theta_phys) && (theta_phys < turnphys4)) ? slopephys3 : physical_disutility);
+    physical_disutility = interceptphys4 + theta_phys * (((turnphys4 <= theta_phys) && (theta_phys < turnphys5)) ? slopephys4 : physical_disutility);
+    physical_disutility = interceptphys5 + theta_phys * (((turnphys5 <= theta_phys) && (theta_phys < turnphys6)) ? slopephys5 : physical_disutility);
+    physical_disutility = interceptphys6 + theta_phys * (((turnphys6 <= theta_phys) && (theta_phys < turnphys7)) ? slopephys6 : physical_disutility);
+    physical_disutility = interceptphys7 + theta_phys * (((turnphys7 <= theta_phys) && (theta_phys < turnphys8)) ? slopephys7 : physical_disutility);
+    physical_disutility = interceptphys8 + theta_phys * (((turnphys8 <= theta_phys) && (theta_phys < turnphys9)) ? slopephys8 : physical_disutility);
+    physical_disutility = (turnphys9 <= theta_phys) ? 0 : physical_disutility;
+
+    let sleep_disutility = 0
+    sleep_disutility = interceptsleep1 + theta_slp * (((turnsleep1 <= theta_slp) && (theta_slp < turnsleep2)) ? slopesleep1 : sleep_disutility);
+    sleep_disutility = interceptsleep2 + theta_slp * (((turnsleep2 <= theta_slp) && (theta_slp < turnsleep3)) ? slopesleep2 : sleep_disutility);
+    sleep_disutility = interceptsleep3 + theta_slp * (((turnsleep3 <= theta_slp) && (theta_slp < turnsleep4)) ? slopesleep3 : sleep_disutility);
+    sleep_disutility = interceptsleep4 + theta_slp * (((turnsleep4 <= theta_slp) && (theta_slp < turnsleep5)) ? slopesleep4 : sleep_disutility);
+    sleep_disutility = interceptsleep5 + theta_slp * (((turnsleep5 <= theta_slp) && (theta_slp < turnsleep6)) ? slopesleep5 : sleep_disutility);
+    sleep_disutility = interceptsleep6 + theta_slp * (((turnsleep6 <= theta_slp) && (theta_slp < turnsleep7)) ? slopesleep6 : sleep_disutility);
+    sleep_disutility = interceptsleep7 + theta_slp * (((turnsleep7 <= theta_slp) && (theta_slp < turnsleep8)) ? slopesleep7 : sleep_disutility);
+    sleep_disutility = (turnsleep8 <= theta_slp) ? 1 : sleep_disutility;
+
+    let social_disutility = 1
+    social_disutility = interceptsocial1 + theta_sr * (((turnsocial1 <= theta_sr) && (theta_sr < turnsocial2)) ? slopesocial1 : social_disutility);
+    social_disutility = interceptsocial2 + theta_sr * (((turnsocial2 <= theta_sr) && (theta_sr < turnsocial3)) ? slopesocial2 : social_disutility);
+    social_disutility = interceptsocial3 + theta_sr * (((turnsocial3 <= theta_sr) && (theta_sr < turnsocial4)) ? slopesocial3 : social_disutility);
+    social_disutility = interceptsocial4 + theta_sr * (((turnsocial4 <= theta_sr) && (theta_sr < turnsocial5)) ? slopesocial4 : social_disutility);
+    social_disutility = interceptsocial5 + theta_sr * (((turnsocial5 <= theta_sr) && (theta_sr < turnsocial6)) ? slopesocial5 : social_disutility);
+    social_disutility = interceptsocial6 + theta_sr * (((turnsocial6 <= theta_sr) && (theta_sr < turnsocial7)) ? slopesocial6 : social_disutility);
+    social_disutility = interceptsocial7 + theta_sr * (((turnsocial7 <= theta_sr) && (theta_sr < turnsocial8)) ? slopesocial7 : social_disutility);
+    social_disutility = interceptsocial8 + theta_sr * (((turnsocial8 <= theta_sr) && (theta_sr < turnsocial9)) ? slopesocial8 : social_disutility);
+    social_disutility = (turnsocial9 <= theta_sr) ? 0 : social_disutility;
+
+    const multi_attribute_disutility = (1 / C) * ((1 + C * c_cognition * cog_disutility) * (1 + C * c_depression * dep_disutility) * (1 + C * c_fatigue * fat_disutility) * (1 + C * c_pain * pain_disutility) * (1 + C * c_physical * physical_disutility) * (1 + C * c_sleep * sleep_disutility) * (1 + C * c_social * social_disutility) - 1)
+
+    const PROPr = (1 - to_dead * multi_attribute_disutility).toFixed(3);
+
+    const cognition_utility = (1 - cog_disutility).toFixed(3);
+    const depression_utility = (1 - dep_disutility).toFixed(3);
+    const fatigue_utility = (1 - fat_disutility).toFixed(3);
+    const pain_utility = (1 - pain_disutility).toFixed(3);
+    const physical_utility = (1 - physical_disutility).toFixed(3);
+    const sleep_utility = (1 - sleep_disutility).toFixed(3);
+    const social_utility = (1 - social_disutility).toFixed(3);
+
+    return {
+        PROPr,
+        cognition_utility,
+        depression_utility,
+        fatigue_utility,
+        pain_utility,
+        physical_utility,
+        sleep_utility,
+        social_utility,
+    }
+}
+
+export function proprScoreFromTScores(
+    t_dep: number,
+    t_fat: number,
+    t_pain: number,
+    t_phys: number,
+    t_slp: number,
+    t_sr: number,
+    t_ax: number,
+    t_cog: number
+) {
+    const theta_dep = (t_dep - 50) / 10;
+    const theta_fat = (t_fat - 50) / 10;
+    const theta_pain = (t_pain - 50) / 10;
+    const theta_phys = (t_phys - 50) / 10;
+    const theta_slp = (t_slp - 50) / 10;
+    const theta_sr = (t_sr - 50) / 10;
+    const theta_ax = (t_ax - 50) / 10;
+    const theta_cog = 0.009 + (-0.037) * theta_dep + 0.118 * theta_phys + (-0.223) * theta_slp + 0.051 * theta_sr + (-0.168) * theta_ax;
+
+    return propsScore(theta_dep, theta_fat, theta_pain, theta_phys, theta_slp, theta_sr, theta_cog)
+}
